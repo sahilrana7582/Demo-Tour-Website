94e294ee08ffb9fc47ebd28e2f30d7a79ab59202
Sahil Rana <krishna@Mac.lan>
2024-11-15 22:33:33 +0530

    Demo Website

--- Backend/app.js
+++ Backend/app.js
@@ -6,12 +6,18 @@ const morgan = require('morgan');
 const dbConnect = require('./database/dbConnect');
 const app = express();
 const AppError = require('./utils/AppError');
+const cors = require('cors');
 
 //Middlewares
 dbConnect();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 const PORT = process.env.PORT;
 
--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -4,6 +4,7 @@ exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
   res.status(200).json({
     status: 'Sucess',
+    results: users.length,
     data: {
       users,
     },
